787d568cd9ee70c680019871e9d2c0ef33c199d2
Noah Fultz <noah.fultz314@gmail.com>
2023-11-04 21:05:14 -0400

    Update playlistParse.js
    Created a class for storing attributes of songs in a playlist, and began blocking in functionality for extracting and formatting information.

--- javascript/playlistParse.js
+++ javascript/playlistParse.js
@@ -1,3 +1,77 @@
+class Song {
+	#title;
+	#artist;
+	#album;
+	#length;
+	#explicit;
+	#track;
+	#disk;
+	
+	constructor(title, artist, album, rawLen, explicit, track, disk) {
+		this.title    = title;
+		this.artist   = artist;
+		this.album    = album;
+		this.length   = rawLen;
+		this.explicit = explicit;
+		this.track    = track;
+		this.disk     = disk;
+	}
+
+	//Mutators
+	setTitle(inTitle)    {this.title    = inTitle;}
+	setArtist(inArtist)  {this.artist   = inArtist;}
+	setAlbum(inAlbum)    {this.album    = inAlbum;}
+	setRawLen(inLen)     {this.length   = inLen;}
+	makeExplicit()       {this.explicit = true;}
+	makeNotExplicit()    {this.explicit = false;}
+	setTrackNum(inVal)   {this.track    = inVal;}
+	setDiskNum(inVal)    {this.disk     = inVal;}
+	makeSingle()         {this.track    = 1;
+						  this.disk     = 1;}
+
+	//Accessors
+	getTitle()   {return this.title;}
+	getAlbum()   {return this.album;}
+	isExplicit() {return this.explicit;}
+	getTrack()   {return this.track;}
+	getDisk()    {return this.disk};
+	getRawLen()  {return this.len};
+	getLength()  {let outText = "";
+				  let lenSec  = this.len;
+				  let lenMin  = 0;
+				  let lenHrs  = 0;
+				  
+				  lenMin = Math.floor(lenSec / 60);
+				  lenSec = lenSec % 60;
+				  lenHrs = Math.floor(lenMin / 60);
+				  lenMin = lenMin % 60;
+				  
+				  if (lenHrs < 10) {outText += "0";}
+				  outText += lenHrs;
+				  outText += ":";
+				  if (lenMin < 10) {outText += "0";}
+				  outText += lenMin;
+				  outText += ":";
+				  if (lenSec < 10) {outText += "0";}
+				  outText += lenSec;
+				  
+				  return outText;
+				 };
+
+};
+
+var song1 = new Song("Shooting Star", "MUNA", "MUNA", 232, false, 11, 1);
+var song2 = new Song("Where is Everyone?", "Lunar Vacation", "Inside Every Fig is a Dead Wasp", 178, false, 4, 1);
+var song3 = new Song("Lonesome Love", "Mitski", "Be the Cowboy", 113, true, 5, 1);
+var song4 = new Song("Desde Que", "Liquits", "Jardín", 217, false, 3, 1);
+var song5 = new Song("Night Eyes", "The Orion Experience", "Fever Dream", false, 3, 1);
+
+var track1 = new Song("2 + 2 = 5", "Radiohead", "Hail to the Theif", 199, false, 1, 1);
+var track2 = ["I Might be Wrong", "Radiohead", "Amnesiac", 293, false, 5, 1];
+
+var TDH_Week_5 = [song1, song2, song3, song4, song5];
+var Radiohead_tries_to_do_math = [track1, track2];
+
 function getInfo() {
 	
 	var playlistURL = document.getElementById("URLInput").value;
@@ -10,7 +84,7 @@ function getInfo() {
 		creator        = "B";
 		totLen_seconds = 7689;
 	} else if (playlistURL == "https://github.com/Asmoranomardicadaistinaculdicar/Playlist2Print-site") {
-		playlistName   = "Radiohead tries to do Math"
+		playlistName   = "Radiohead tries to do math"
 		creator        = "four-caddit";
 		totLen_seconds = 480;
 	}
@@ -45,7 +119,14 @@ function getInfo() {
 	lenString += len_seconds;
 	document.getElementById("outputHeader").innerHTML = playlistName + " by " + creator + ", length: " + lenString + "<br>";
 	
+	let currPlaylist;
+	if (playlistName === "TDH Week 5") {
+		currPlaylist = TDH_Week_5;
+	} else if (playlistName === "Radiohead tries to do math") {
+		currPlaylist = Radiohead_tries_to_do_math;
+	}
 	
+	alert("Playlist length: " + currPlaylist.length + " songs");
 }
 
 function updateDropdown() {
